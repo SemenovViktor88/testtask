0be4ce9b11ff964267102a71bcdeec46d4b7cf31
Viktor Semenov <viktorsemenov88@gmail.com>
2025-03-27 17:13:24 +0200

    refactor build.gradle.kts/data/domain

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -7,24 +7,8 @@ plugins {
 
 android {
     namespace = "com.semenov.domain"
-    compileSdk = 35
+    compileSdk = Android.compileSdk
 
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -39,8 +23,6 @@ dependencies {
     ksp(libs.hilt.android.compiler)
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
